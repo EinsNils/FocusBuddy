11bdca38851e588fdd1f371f1f586f1ddf41617b
Nils Zentgraf <nils.zentgraf@aol.com>
2024-06-22 21:26:47 +0200

    Initial Commit

---
+++ FocusBuddy/FocusBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBuddyApp.swift
+//  FocusBuddy
+//
+//  Created by Nils Zentgraf on 22.06.24.
+//
+
+import SwiftUI
+
+@main
+struct FocusBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
